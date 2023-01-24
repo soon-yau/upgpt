aeb2a2b5df051e41fd941522f502febe128a4e43
soon-yau <soonyau@gmail.com>
2023-01-24 09:57:31 +0000

    merge fid to 1 script

--- scripts/eval_metrics.py
+++ scripts/eval_metrics.py
@@ -15,6 +15,7 @@ from pytorch_msssim import ssim
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import lpips
+import subprocess
 
 
 def get_parser():
@@ -67,42 +68,52 @@ class Loader(Dataset):
         return gt, sample, file_name
 
 
-fnames = []
-ssim_scores = []
-#lpips_alex = []
-lpips_vgg = []
+def eval(gt_dir, sample_dir, batch_size, gpu=0):
 
-parser = get_parser()
-args = parser.parse_args()
+    fnames = []
+    ssim_scores = []
+    #lpips_alex = []
+    lpips_vgg = []
 
-device = f'cuda:{args.gpu}'
-#loss_fn_alex = lpips.LPIPS(net='alex').to(device)
-loss_fn_vgg = lpips.LPIPS(net='vgg').to(device)
+    device = f'cuda:{gpu}'
+    #loss_fn_alex = lpips.LPIPS(net='alex').to(device)
+    loss_fn_vgg = lpips.LPIPS(net='vgg').to(device)
+    #ret = subprocess.Popen(['python','-m', 'pytorch_fid', gt_dir, sample_dir])
+    ret = os.popen(f"python -m pytorch_fid {gt_dir} {sample_dir}")  
+    fid_str = ret.read()
 
-loader = iter(DataLoader(Loader(args.gt_dir, args.sample_dir),
-                    batch_size=args.batch_size, shuffle=False))
-for gt, sample, fname in tqdm(loader):
-    fnames.extend(fname)
-    sample = sample.to(device)
-    gt = gt.to(device)
-    ssim_scores.extend(ssim(sample, gt, data_range=1, size_average=False).cpu().numpy().tolist())
-    lpips_vgg.extend(loss_fn_vgg(sample, gt).view(-1).detach().cpu().numpy().tolist())
-
-
-df = pd.DataFrame({'name':fnames, 
-                    'ssim':ssim_scores,
-                    #'lpips_alex': lpips_alex,
-                    'lpips_vgg': lpips_vgg})
-log_dir = Path(args.sample_dir)/'../'
-df.to_csv(log_dir/'metrics.csv', index=False)
-
-text_file = open(str(log_dir/'metrics.txt'), "w")
-for metric in ['ssim', 'lpips_vgg']:
-    line = f"{metric}: {df[metric].mean()}"
-    print(line)
-    text_file.write(line+'\n')
-text_file.close()
-    
+    loader = iter(DataLoader(Loader(gt_dir, sample_dir),
+                        batch_size=batch_size, shuffle=False))
+    for gt, sample, fname in tqdm(loader):
+        fnames.extend(fname)
+        sample = sample.to(device)
+        gt = gt.to(device)
+        ssim_scores.extend(ssim(sample, gt, data_range=1, size_average=False).cpu().numpy().tolist())
+        lpips_vgg.extend(loss_fn_vgg(sample, gt).view(-1).detach().cpu().numpy().tolist())
 
 
+    df = pd.DataFrame({'name':fnames, 
+                        'ssim':ssim_scores,
+                        #'lpips_alex': lpips_alex,
+                        'lpips_vgg': lpips_vgg})
+    log_dir = Path(sample_dir)/'../'
+    df.to_csv(log_dir/'metrics.csv', index=False)
 
+
+    text_file = open(str(log_dir/'metrics.txt'), "w")
+    print('\n'+fid_str)    
+    text_file.write(fid_str)
+
+    for metric in ['ssim', 'lpips_vgg']:
+        line = f"{metric}: {df[metric].mean()}"
+        print(line)
+        text_file.write(line+'\n')
+
+    text_file.close()
+        
+
+if __name__ == '__main__':
+
+    parser = get_parser()
+    args = parser.parse_args()
+    eval(args.gt_dir, args.sample_dir, args.batch_size, args.gpu)
